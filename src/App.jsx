64c5e161701b5c8204762cb5682cfc4e702239a8
Vignesh Sadhu <vigneshsadhu143@gmail.com>
2024-01-06 12:58:49 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import AddTodo from '../src/components/AddTodo.jsx'
+import Todos from './components/Todos.jsx'
+function App() {
+
+
+  return (
+    <>
+      <AddTodo />
+      <Todos/>
+    </>
+  )
+}
+
+export default App
